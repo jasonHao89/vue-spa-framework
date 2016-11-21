798cf2f87b993cedbd2e57ee93f1b65a1fd00dcc
HaoSijie <414117934@qq.com>
2016-11-21 17:41:14 +0800

    建立外部引入css与js

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,9 @@
+<style lang="css" src="./index.css" scoped></style>
 <template>
     <div>
         我是index
+        <pre>{{$data}}</pre>
     </div>
 </template>
+<script src="./index.js"></script>
+
