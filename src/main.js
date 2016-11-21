e2ab92c1b03fefd2528c3811addb4262a3ce7cbf
HaoSijie <414117934@qq.com>
2016-11-21 15:30:33 +0800

    初步部署vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
 })
+
+new Vue({
+  router
+}).$mount('#app')
+
