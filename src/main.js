c892bb067173cc2b19e23e15e8a7e26c7d18865d
HaoSijie <414117934@qq.com>
2016-11-21 15:47:54 +0800

    调试成功第一版vue－router

--- src/main.js
+++ src/main.js
@@ -3,16 +3,15 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
 
 const routes = [
-  { path: '/foo', component: Foo },
+  { path: '/', component: require('./views/index.vue') },
   { path: '/bar', component: Bar }
 ]
 
 const router = new VueRouter({
-  routes // （缩写）相当于 routes: routes
+  routes
 })
 
 new Vue({
