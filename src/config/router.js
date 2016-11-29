ef7aeac47ec207a23ee36edff5d69168bfad5792
HaoSijie <414117934@qq.com>
2016-11-29 10:30:24 +0800

    模块化router配置

---
+++ src/config/router.js
@@ -0,0 +1,18 @@
+export default function (VueRouter) {
+  const routes = [
+    { path: '/', component: require('../views/index.vue') },
+    { path: '/foo', component: require('../views/foo.vue') }
+  ]
+  const router = new VueRouter({
+    mode: 'history',
+    routes
+  })
+  router.beforeEach((to, from, next) => {
+    console.warn('beforeEach')
+    next()
+  })
+  router.afterEach(route => {
+    console.warn('afterEach')
+  })
+  return router
+}
--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Router from './config/router'
 import Resource from 'vue-resource'
 Vue.use(VueRouter)
 Vue.use(Resource)
-
-const routes = [
-  { path: '/', component: require('./views/index.vue') },
-  { path: '/foo', component: require('./views/foo.vue') }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  routes
-})
-
-router.beforeEach((to, from, next) => {
-  console.warn('beforeEach')
-  next()
-})
-router.afterEach(route => {
-  console.warn('afterEach')
-})
+const router = Router(VueRouter)
 new Vue({
   router
 }).$mount('#app')
