cb6c90a15204c4a2f79222935bc5a0b2fcaf5294
HaoSijie <414117934@qq.com>
2016-12-01 13:16:46 +0800

    添加jquery-validation自定义验证声明文件

---
+++ src/assets/additional-methods.js
@@ -0,0 +1,4 @@
+import $ from 'jquery'
+import 'jquery-validation/dist/additional-methods'
+// 自定义全局验证方法
+module.exports = $
--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import Router from './config/router'
 import Resource from 'vue-resource'
 import 'jquery' // assets/jquery.js
 import 'jquery-validation'
+import './assets/additional-methods.js'
 /* $ 已全局化, 因为eslint规范变量必须先声明在使用，所以请使用window.$
 * 建议写法:
 * var $ = window.$
 * .........
 */
-
+console.warn(window.$().validate)
 Vue.use(VueRouter)
 Vue.use(Resource)
 const router = Router(VueRouter)
