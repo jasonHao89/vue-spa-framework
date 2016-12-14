e85e966a67e5e7ba418d1b4ca383d98d55fc4ec0
HaoSijie <414117934@qq.com>
2016-12-14 12:29:27 +0800

    添加hide与Route过滤情

--- src/config/vue-filters.js
+++ src/config/vue-filters.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import {Route} from '../config/classes.js'
 export default (()=>{
     Vue.filter('date_format',(value,format)=>{
         const date=new Date(value)
         return date.format(format)
+    })
+    Vue.filter('hide',()=>{
+        return ''
+    })
+    Vue.filter('Route',(route)=>{
+        route=new Route(route)
+        return ''
     })
 })()
